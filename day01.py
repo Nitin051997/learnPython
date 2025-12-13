469f71ec7ba163cbbe894851690d057d1e1e3a83
unknown <nitin.nadar5@gmail.com>
2025-12-13 23:54:30 +0530

    User Input in PY

--- day01.py
+++ day01.py
@@ -1,3 +1,6 @@
+#Learn printing, comments, variables, data types
+
+
 print('Hello World')
 print(30)
 print(2+3)
@@ -42,3 +45,18 @@ print(data_array)
 
 print(type(data_object))
 print(type(data_array))
+
+#Take user input
+
+user_name = input("Enter your Name: ")
+user_age = int(input("Enter your Age: "))
+user_height = float(input("Enter your Height: "))
+print(user_height)
+
+system_name, system_type = input("Enter System Name & Type: ").split()
+print(system_name, system_type)
+
+
+#Add Two Number
+a, b = map(int, input("Enter Two Number: ").split())
+print(a + b)
