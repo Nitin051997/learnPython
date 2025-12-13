e7f7414088dc6cd47f905b64a3b11168a4042545
unknown <nitin.nadar5@gmail.com>
2025-12-13 23:20:20 +0530

    day 01 -- file

---
+++ day01.py
@@ -0,0 +1,44 @@
+print('Hello World')
+print(30)
+print(2+3)
+
+
+name = 'Nitin'
+age = 28
+print(name, age)
+
+height = 176.7
+alive = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(alive))
+
+
+#Practice session 
+
+name = "Nitin"                  #string
+age = 28                        #int
+role = "Frontend Developer"     #string
+is_working = True               #boolean
+
+
+print("Name: ", name)
+print("Age: ", age)
+print("Role: ", role)
+print("Working: ", is_working)
+
+#Can we store an array ??
+
+data_object = {name: "nitin"}       #dict
+data_array = [{name: "nitin"}]      #list
+
+print(data_object)
+print(data_array)
+
+#Yes we can!
+#What will be it's type?
+
+print(type(data_object))
+print(type(data_array))
